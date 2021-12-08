95b6ace925e16bdb857c2551691ea3394220ba1b
sunilk901 <kumarm.1@tcs.com>
2021-12-08 15:38:15 +0530

    ApiCloudGW app first commit

---
+++ src/main/java/com/tcs/apicloudgateway/ApiCloudGatewayApplication.java
@@ -0,0 +1,17 @@
+package com.tcs.apicloudgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
+public class ApiCloudGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCloudGatewayApplication.class, args);
+	}
+
+}
